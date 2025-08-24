fbbd7ecf24571e0a3357d0d8e85c6a20e92eaf98
meb-14 <yone02140914@gmail.com>
2025-08-24 17:24:46 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btn');
 
-const textContent = document.getElementById('text');
+const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
     text.textContent = 'ボタンをクリックしました';
